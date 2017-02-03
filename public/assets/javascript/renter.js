afa9f8a34e0f5751b57d7f6bf95a906cd7b30a67
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-02-03 17:18:30 -0600

    adds working method to place all the markers on the renters google maps, adds geolocation centering on map load

--- public/assets/javascript/renter.js
+++ public/assets/javascript/renter.js
@@ -5,10 +5,76 @@ function initMap() {
         zoom: 13,
         center: myLatLng
     });
+    var infoWindow = new google.maps.InfoWindow({ map: map });
 
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'Hello World!'
-    });
+
+
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Current Location');
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
+    }
+
+
+
+
+    // Create all markers for rentals
+    var allMarkers = locations();
+    var infowindow = new google.maps.InfoWindow();
+    var marker, i;
+
+    for (i = 0; i < allMarkers.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(allMarkers[i].lat, allMarkers[i].lon),
+            map: map
+        });
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(allMarkers[i].name);
+                infowindow.open(map, marker);
+            };
+        })(marker, i));
+    }
+
+}
+
+// this will be a sequelize query for all records lat long
+function locations() {
+    var locations = [{
+        name: 'Northwestern SoPS',
+        lat: '41.896529',
+        lon: '-87.618727'
+    }, {
+        name: 'Jay Pritzker Pavilion',
+        lat: '41.883159',
+        lon: '-87.621892'
+    }, {
+        name: 'theMART',
+        lat: '41.888512',
+        lon: '-87.635435'
+    }];
+    return locations;
 }
