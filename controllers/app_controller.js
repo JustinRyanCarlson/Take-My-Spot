111c0d3afd0d4d41cc1b07116c7a5622047de846
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-02-02 20:43:48 -0600

    adds login and newuser routes to app_controller.js, adds some base styling to the handlebars files so i can see if stuff is working, adds some javascript to main.handlebars to post data on button click (will be moved to public), deletes some columns from renter.js (still not sure what will go in here

--- controllers/app_controller.js
+++ controllers/app_controller.js
@@ -4,10 +4,24 @@ var db = require("../models");
 
 
 // ROUTES
+
+router.post("/newuser", function(req, res) {
+    console.log(req.body);
+    db.users.create({
+        email: req.body.email,
+        password: req.body.password,
+        name: req.body.name,
+        phone: req.body.phone
+    });
+});
+
+router.post("/login", function(req, res) {
+    console.log('success');
+});
+
 router.use(function(req, res) {
     res.render('login.handlebars');
 });
-
 
 // Export routes for server.js to use.
 module.exports = router;
