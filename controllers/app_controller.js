ea9c9a81c9f2ca14ad0564365160f9ec76a25dab
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-02-03 18:20:11 -0600

    adds additional handlebars scripts to be handed in on render since multiple google maps scripts on the same page were having conflicts, adds owner.js with a little form with google auto complete

--- controllers/app_controller.js
+++ controllers/app_controller.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../models");
+var script = {
+    login: '<script src="javascript/login.js" type="text/javascript"></script>',
+    owner: '<script src="javascript/owner.js" type="text/javascript"></script><script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkjQIFfTlx7SAlf71jK9wgvWj6-Urkamc&libraries=places&callback=initAutocomplete" async defer></script>',
+    renter: '<script src="javascript/renter.js" type="text/javascript"></script><script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkjQIFfTlx7SAlf71jK9wgvWj6-Urkamc&callback=initMap"></script>'
+};
 
 
 // ROUTES
@@ -21,11 +26,16 @@ router.post("/login", function(req, res) {
 
 // need to add a user placeholder to this route
 router.get('/renter', function(req, res) {
-    res.render('renter.handlebars', { title: 'TMS | Rentals' });
+    res.render('renter.handlebars', { title: 'TMS | Rentals', scripts: script.renter });
+});
+
+// need to add a user placeholder to this route
+router.get('/owner', function(req, res) {
+    res.render('owner.handlebars', { title: 'TMS | Owner', scripts: script.owner });
 });
 
 router.use(function(req, res) {
-    res.render('login.handlebars', { title: 'TMS | Welcome' });
+    res.render('login.handlebars', { title: 'TMS | Welcome', scripts: script.login });
 });
 
 // Export routes for server.js to use.
